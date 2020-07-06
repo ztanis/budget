082ce0e39a25c053632e89c794d799bf45aea902
Valentin Mouret <valentin.mout@gmail.com>
2020-07-06 18:46:32 +0200

    Add N26 importer

--- budget/sources/factory.py
+++ budget/sources/factory.py
@@ -1,10 +1,12 @@
 from budget.sources.base import BaseImporter
 from budget.sources.cb import CBImporter
+from budget.sources.n26 import N26Importer
 
 
 class Factory:
     _importers = {
-        'cb': CBImporter()
+        "cb": CBImporter(),
+        "n26": N26Importer()
     }
 
     @classmethod
